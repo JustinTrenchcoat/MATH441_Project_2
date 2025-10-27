3ad9abb3167ac68f3d2b8bccc9201c940f63c7c3
JustinTrenchcoat <justinchang2021@outlook.com>
2025-10-26 23:40:45 -0400

    organized code

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # easy function for updating the status
 def Update_EV(counter_EV):
@@ -29,21 +32,21 @@ def check_price(i, fuel, imported, profit, EV_fee, license_fee, counter_EV, coun
     
 def infoPage(N, profit, size, budget, parkCapacity, spent):
     print("Total Number of cars ready to sell: ", N)
-    print("Total car value: $",np.sum(profit))
-    print("Total Size of cars: ",size, "m^2")
-    print("Total buying cost amount: $", sum(budget))
+    print("Total car value: $",np.sum(profit).round(2))
+    print("Total Size of cars: ",size.round(2), "m^2")
+    print("Total buying cost amount: $", sum(budget).round(2))
     print("------------------------------------------")
-    print("Parking Lot Capacity: ",parkCapacity, "m^2")
-    print("Available Budget Amount: $", spent)
+    print("Parking Lot Capacity: ",parkCapacity.round(2), "m^2")
+    print("Available Budget Amount: $", spent.round(2))
 
 def runOG(items, profit, size, budget):
     print("---------------------------------------------------------------")
     print("Dealership Decision Optimizer Greedy System(D-DOGS) is optimizing....")
     print("---------------------------------------------------------------")
     print(len(items), "cars sold")
-    print("Profit: $", sum(profit[items]))
-    print("Actual Size of cars sold: ", sum(size[items]), "m^2")
-    print("Actual budget spent: $", sum(budget[items]))
+    print("Profit: $", sum(profit[items]).round(2))
+    print("Actual Size of cars sold: ", sum(size[items]).round(2), "m^2")
+    print("Actual budget spent: $", sum(budget[items]).round(2))
 
 
 # check if the car i is EV or imported
@@ -61,60 +64,18 @@ def check_constraints(items, size, cost, parkCapacity, dealBudget):
         return True
     else:
         return False
-    
-def ddog_size(items,fuel, imported, 
-              value_per_size,
-              profit, EV_fee, license_fee, 
-              size, cost, parkCapacity,dealBudget, 
-              counter_EV, counter_import, should_break):
-    while True:
-        # Find the most valuable item
-        sorted_indices = value_per_size.sort_values(ascending=False).index.tolist() # Indices sorted in descending order
-        i = sorted_indices[0]  # Index of the largest item
-        j = sorted_indices[1]    
-        if value_per_size[i] == 0:
-            break
-        else:
-            if (check_price(i,fuel, imported, 
-                            profit, EV_fee, license_fee, 
-                            counter_EV, counter_import) >= check_price(j,fuel, imported, 
-                                                                        profit, EV_fee, license_fee, 
-                                                                        counter_EV, counter_import)):
-                items.append(i)
-                # update the counter if necessary
-                if check_EV(i, fuel):
-                    Update_EV(counter_EV)
-                elif check_import(i, imported):
-                    Update_Import(counter_import)
-                value_per_size[i] = 0  
-                if(check_constraints(items, size, cost, parkCapacity, dealBudget)):
-                    should_break=True
-            else:
-                items.append(j)
-                # update the counter if necessary
-                if check_EV(j, fuel):
-                    Update_EV(counter_EV)
-                elif check_import(j, imported):
-                    Update_Import(counter_import)
-                value_per_size[j] = 0  
-                if(check_constraints(items, size, cost,parkCapacity, dealBudget)):
-                    should_break=True
-        if (should_break):
-            break
-    return items
 
-
-def ddog_budget(items,fuel, imported, 
-              value_per_budget,
+def greedy(items,fuel, imported, 
+              valList,
               profit, EV_fee, license_fee, 
               size, cost, parkCapacity,dealBudget,
               counter_EV, counter_import, should_break):
     while True:
         # Find the most valuable item
-        sorted_indices = value_per_budget.sort_values(ascending=False).index.tolist() # Indices sorted in descending order
+        sorted_indices = valList.sort_values(ascending=False).index.tolist() # Indices sorted in descending order
         i = sorted_indices[0]  # Index of the largest item
         j = sorted_indices[1]    
-        if value_per_budget[i] == 0:
+        if valList[i] == 0:
             break
         else:
             if (check_price(i,fuel, imported, 
@@ -128,7 +89,7 @@ def ddog_budget(items,fuel, imported,
                     Update_EV(counter_EV)
                 elif check_import(i, imported):
                     Update_Import(counter_import)
-                value_per_budget[i] = 0  
+                valList[i] = 0  
                 if(check_constraints(items, size, cost, parkCapacity, dealBudget)):
                     should_break=True
             else:
@@ -138,9 +99,40 @@ def ddog_budget(items,fuel, imported,
                     Update_EV(counter_EV)
                 elif check_import(j, imported):
                     Update_Import(counter_import)
-                value_per_budget[j] = 0  
+                valList[j] = 0  
                 if(check_constraints(items, size, cost, parkCapacity, dealBudget)):
                     should_break=True
         if (should_break):
             break
     return items
+
+
+def selected(df, index,alg_name, title, feature, select_type):
+    # mare sure the catagories:
+    catagories = df[select_type].unique()
+    # specify the summarize type
+    if select_type == 'imported':
+        # we are doing the import/demestic summary:
+        selected = df.loc[index, select_type].map({0: 'domestic', 1: 'imported'})
+    else:
+        selected = pd.Categorical(df.loc[index, select_type], categories=catagories, ordered=True)  
+    # continue with summary, we don't want to list up all the brands:
+    if select_type != "brand":
+        print(f"Summarize of profit, catagorized in {feature}:")
+        for catagory in catagories:
+            type_profit = df[df[select_type].isin([catagory])]['Profit'].mean().round(1)
+            print(f"Mean profit for {catagory} cars: {type_profit}")
+    #-----------------------------------------------------------------------------
+    counts = pd.Series(selected).value_counts().sort_index()
+    plt.figure(figsize=(14,5))
+    counts.plot(kind="bar", color = "skyblue", edgecolor="black")
+    plt.xlabel(feature)
+    plt.ylabel("Count")
+    plt.title(f"Distribution of {title} Chosen {feature}")
+    plt.xticks(rotation=45, ha="right", fontsize=10)  # Reduce font size
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+    plt.subplots_adjust(bottom=0.2)  # Add padding to prevent label cutoff
+    for i, value in enumerate(counts):
+        plt.text(i,value +0.5, str(value), ha="center", va="bottom", fontsize=10)
+    plt.savefig(f"data/{alg_name}_{select_type}.png", dpi=300, bbox_inches="tight")
+    plt.show()
